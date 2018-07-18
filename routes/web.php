a4b6054af33b2a2a7631085e84be0f177b514452
famousmai <906631095@qq.com>
2018-07-18 15:57:11 +0800

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 首页
+Route::get('/', 'StaticPagesController@home');
+// 帮助
+Route::get('/help', 'StaticPagesController@help');
+// 关于我们
+Route::get('/about', 'StaticPagesController@about');
